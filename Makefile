44aabeff39da2f78de230e6f7c907db8e673656b
Nobuyuki Matsumoto <mtsmtnbyk@gmail.com>
2025-11-22 15:45:04 -0500

    dual ok

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CXX=g++
 CXXFLAGS=-Wall -Wextra -std=c++23 -march=native -O3
-# INCLUDES=-I"/mnt/hdd_barracuda/opt/eigen/" -I"/mnt/hdd_barracuda/opt/HighFive/include/" -I"/mnt/hdd_barracuda/opt/hdf5-v1.14.0/include/" -I"~/opt/eigen/"
-# LDFLAGS=-L"/mnt/hdd_barracuda/opt/hdf5-v1.14.0/lib/" -L"/usr/lib/" -L"/usr/local/lib/" -lhdf5 # -lhdf5_cpp
-INCLUDES=-I"/Users/nobuyukimatsumoto/opt/eigen/"
-LDFLAGS=-L"/usr/local/lib/" -lgsl -lgslcblas -lm
+INCLUDES=-I"/mnt/hdd_barracuda/opt/eigen/" -I"/mnt/hdd_barracuda/opt/HighFive/include/" -I"/mnt/hdd_barracuda/opt/hdf5-v1.14.0/include/" -I"~/opt/eigen/"
+LDFLAGS=-L"/mnt/hdd_barracuda/opt/hdf5-v1.14.0/lib/" -L"/usr/lib/" -L"/usr/local/lib/" -lhdf5 -lgsl -lgslcblas -lm # -lhdf5_cpp
+# INCLUDES=-I"/Users/nobuyukimatsumoto/opt/eigen/"
+# LDFLAGS=-L"/usr/local/lib/" -lgsl -lgslcblas -lm
 
 all:
 	$(CXX) geometry.cc $(CXXFLAGS) $(INCLUDES) $(LDFLAGS) -o geometry.o
--- geometry.cc
+++ geometry.cc
@@ -479,6 +479,23 @@ struct RefinedIcosahedron {
   }
 
   // only defined for regular points
+  void shiftMX( Coords& np,
+                const Coords& n ) const {
+    const int x = n[0];
+    const int y = n[1];
+    const int s = n[2];
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+
+    if(x==0){ // steps over the patch bdy
+      if(s<NPatches/2) np = Coords{L-1, y, (s+4)%(NPatches/2)+NPatches/2}; // southern hemisphere
+      else np = Coords{L-1-y, L-1, (s-1)%(NPatches/2)+NPatches/2}; // northern hemisphere
+    }
+    else np = Coords{x-1, y, s}; // within patch
+  }
+
+  // only defined for regular points
   void shiftPY( Coords& np,
                 const Coords& n ) const {
     const int x = n[0];
@@ -496,6 +513,23 @@ struct RefinedIcosahedron {
       }
     }
     else np = Coords{x, y+1, s}; // within patch
+  }
+
+  // only defined for regular points
+  void shiftMY( Coords& np,
+                const Coords& n ) const {
+    const int x = n[0];
+    const int y = n[1];
+    const int s = n[2];
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+
+    if(y==0){ // steps over the patch bdy
+      if(s<NPatches/2) np = Coords{L-1, L-1-x, (s-1+NPatches/2)%(NPatches/2)}; // southern hemisphere
+      else np = Coords{x, L-1, (s-5)%(NPatches/2)}; // northern hemisphere
+    }
+    else np = Coords{x, y-1, s}; // within patch
   }
 
 
@@ -518,6 +552,34 @@ struct RefinedIcosahedron {
       if(y==L-1) np = Coords{0, L-1-x, (s+1)%(NPatches/2)+NPatches/2};
       else if(x==L-1) np = Coords{0, y+1, (s-4)%(NPatches/2)};
       else np = Coords{x+1, y+1, s};
+    }
+  }
+
+  // only defined for regular points
+  void shiftMZ( Coords& np,
+                const Coords& n ) const {
+    const int x = n[0];
+    const int y = n[1];
+    const int s = n[2];
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+
+    if(s<NPatches/2){ // southern hemisphere
+      if(x==0) {
+        if(y==0) np = Coords{0, 0, PatchIdxVoid};
+        else np = Coords{L-1, y-1, (s+4)%(NPatches/2)+NPatches/2};
+      }
+      else if(y==0) np = Coords{L-1, L-x, (s-1+NPatches/2)%(NPatches/2)};
+      else np = Coords{x-1, y-1, s};
+    }
+    else { // northern hemisphere
+      if(x==0) {
+        if(y==0) np = Coords{0, 0, PatchIdxVoid};
+        else np = Coords{L-y, L-1, (s-1)%(NPatches/2)+NPatches/2};
+      }
+      else if(y==0) np = Coords{x-1, L-1, (s-5+NPatches/2)%(NPatches/2)};
+      else np = Coords{x-1, y-1, s};
     }
   }
 
@@ -553,8 +615,126 @@ struct RefinedIcosahedronDual {
     return 2*in+n[3];
   }
 
-  // @@
-  // PX, PY, PW, MX, MY, MW
+
+  void shiftPA( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==XZ);
+
+    Coords np;
+    simplicial.shiftPX(np, Coords{x,y,s});
+
+    if(np[2]==PatchIdxS) { // rotate around SP
+      assert(x==L-1);
+      assert(y==0);
+      fp = FaceCoords{x, y, (s+1)%(NPatches/2), XZ};
+    }
+    else if(s<NPatches/2 && x==L-1) fp = FaceCoords{np[0]-1, np[1], np[2], XZ};
+    else fp = FaceCoords{np[0], np[1], np[2], ZY};
+  }
+
+  void shiftMA( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==ZY);
+
+    Coords np;
+    simplicial.shiftMX(np, Coords{x,y,s});
+
+    if(NPatches/2<=s && x==0) fp = FaceCoords{np[0], np[1], np[2], ZY};
+    else fp = FaceCoords{np[0], np[1], np[2], XZ};
+  }
+
+  void shiftPB( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==XZ);
+
+    fp = FaceCoords{x, y, s, ZY};
+  }
+
+  void shiftMB( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==ZY);
+
+    fp = FaceCoords{x, y, s, XZ};
+  }
+
+  void shiftPC( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==XZ);
+
+    Coords np;
+    simplicial.shiftMY(np, Coords{x,y,s});
+
+    if(s<NPatches/2 && y==0) fp = FaceCoords{np[0], np[1], np[2], XZ};
+    else fp = FaceCoords{np[0], np[1], np[2], ZY};
+  }
+
+  void shiftMC( FaceCoords& fp,
+                const FaceCoords& f ) const {
+    const int x = f[0];
+    const int y = f[1];
+    const int s = f[2];
+    const int type = f[3];
+
+    assert(0<=x && x<L);
+    assert(0<=y && y<L);
+    assert(0<=s && s<NPatches);
+    assert(type==ZY);
+
+    Coords np;
+    simplicial.shiftPY(np, Coords{x,y,s});
+
+    // std::cout << "n = " << x << " " << y << " " << s << std::endl;
+    // std::cout << "np = " << np[0] << " " << np[1] << " " << np[2] << std::endl;
+
+    if(np[2]==PatchIdxN) { // rotate around SP
+      assert(y==L-1);
+      assert(x==0);
+      fp = FaceCoords{x, y, (s+1)%(NPatches/2)+(NPatches/2), ZY};
+    }
+    else if(NPatches/2<=s && y==L-1) fp = FaceCoords{np[0], np[1]-1, np[2], ZY};
+    else fp = FaceCoords{np[0], np[1], np[2], XZ};
+  }
+
 
   void FillDualPoints(){
     vertices.clear();
@@ -756,23 +936,55 @@ int main(){
           const V3 vnPY = lattice.vertices[inPY];
           const V3 vnPZ = lattice.vertices[inPZ];
 
-          const double dx = (vn-vnPX).norm();
-          const double dy = (vn-vnPY).norm();
-          const double dz = (vn-vnPZ).norm();
+          {
+            const double dx = (vn-vnPX).norm();
+            const double dy = (vn-vnPY).norm();
+            const double dz = (vn-vnPZ).norm();
 
-          std::cout << "@@ in = " << in << std::endl;
-          // std::cout << "@@ x,y,s = " << x << " " << y << " " << s << std::endl;
-          // std::cout << "+x x,y,s = " << nPX[0] << " " << nPX[1] << " " << nPX[2] << std::endl;
-          // std::cout << "+y x,y,s = " << nPY[0] << " " << nPY[1] << " " << nPY[2] << std::endl;
-          // std::cout << "+z x,y,s = " << nPZ[0] << " " << nPZ[1] << " " << nPZ[2] << std::endl;
-          std::cout << dx << std::endl;
-          std::cout << dy << std::endl;
-          std::cout << dz << std::endl;
+            std::cout << "@@ in = " << in << std::endl;
+            std::cout << "@@ x,y,s = " << x << " " << y << " " << s << std::endl;
+            std::cout << "+x x,y,s = " << nPX[0] << " " << nPX[1] << " " << nPX[2] << std::endl;
+            std::cout << "+y x,y,s = " << nPY[0] << " " << nPY[1] << " " << nPY[2] << std::endl;
+            std::cout << "+z x,y,s = " << nPZ[0] << " " << nPZ[1] << " " << nPZ[2] << std::endl;
+            std::cout << dx << std::endl;
+            std::cout << dy << std::endl;
+            std::cout << dz << std::endl;
 
-          // // for L=2
-          // assert( dx<0.7 );
-          // assert( dy<0.7 );
-          // assert( dz<0.7 );
+            // // for L=2
+            // assert( dx<0.7 );
+            // assert( dy<0.7 );
+            // assert( dz<0.7 );
+          }
+
+          Coords nMX, nMY, nMZ;
+          lattice.shiftMX( nMX, n );
+          lattice.shiftMY( nMY, n );
+          lattice.shiftMZ( nMZ, n );
+
+          if(nMX[2]!=PatchIdxVoid){
+            const Idx inMX = lattice.idx(nMX);
+            const V3 vnMX = lattice.vertices[inMX];
+            const double dx = (vn-vnMX).norm();
+            std::cout << "-x x,y,s = " << nMX[0] << " " << nMX[1] << " " << nMX[2] << std::endl;
+            std::cout << dx << std::endl;
+            // assert( dx<0.7 );
+          }
+          if(nMY[2]!=PatchIdxVoid){
+            const Idx inMY = lattice.idx(nMY);
+            const V3 vnMY = lattice.vertices[inMY];
+            const double dy = (vn-vnMY).norm();
+            std::cout << "-x x,y,s = " << nMY[0] << " " << nMY[1] << " " << nMY[2] << std::endl;
+            std::cout << dy << std::endl;
+            // assert( dy<0.7 );
+          }
+          if(nMZ[2]!=PatchIdxVoid){
+            const Idx inMZ = lattice.idx(nMZ);
+            const V3 vnMZ = lattice.vertices[inMZ];
+            const double dz = (vn-vnMZ).norm();
+            std::cout << "-x x,y,s = " << nMZ[0] << " " << nMZ[1] << " " << nMZ[2] << std::endl;
+            std::cout << dz << std::endl;
+            // assert( dz<0.7 );
+          }
         }}}
   }
 
@@ -794,6 +1006,93 @@ int main(){
 
 
   RefinedIcosahedronDual dual(lattice);
+  {
+    for(int s=0; s<NPatches; s++){
+      for(int x=0; x<L; x++){
+        for(int y=0; y<L; y++){
+          // for(const int type : std::vector<int>{XZ, ZY}){
+          {
+            const int type = XZ;
+            const FaceCoords f{x,y,s,type};
+            const Idx iff = dual.idx(f);
+
+            FaceCoords fPA, fPB, fPC;
+            dual.shiftPA( fPA, f );
+            dual.shiftPB( fPB, f );
+            dual.shiftPC( fPC, f );
+            const Idx ifPA = dual.idx(fPA);
+            const Idx ifPB = dual.idx(fPB);
+            const Idx ifPC = dual.idx(fPC);
+
+            const V3 vf = dual.vertices[iff];
+            const V3 vfPA = dual.vertices[ifPA];
+            const V3 vfPB = dual.vertices[ifPB];
+            const V3 vfPC = dual.vertices[ifPC];
+
+            const double dA = (vf-vfPA).norm();
+            const double dB = (vf-vfPB).norm();
+            const double dC = (vf-vfPC).norm();
+
+            std::cout << "@@ iff = " << iff << std::endl;
+            std::cout << "@@ x,y,s = " << x << " " << y << " " << s << " " << type << std::endl;
+            std::cout << "+A x,y,s = " << fPA[0] << " " << fPA[1] << " " << fPA[2] << " " << fPA[3] << std::endl;
+            std::cout << "+B x,y,s = " << fPB[0] << " " << fPB[1] << " " << fPB[2] << " " << fPB[3] << std::endl;
+            std::cout << "+C x,y,s = " << fPC[0] << " " << fPC[1] << " " << fPC[2] << " " << fPC[3] << std::endl;
+            std::cout << dA << std::endl;
+            std::cout << dB << std::endl;
+            std::cout << dC << std::endl;
+
+            // // for L=2
+            // assert( dA<0.7 );
+            // assert( dB<0.7 );
+            // assert( dC<0.7 );
+          }
+
+          {
+            const int type = ZY;
+            const FaceCoords f{x,y,s,type};
+            const Idx iff = dual.idx(f);
+
+            FaceCoords fMA, fMB, fMC;
+            dual.shiftMA( fMA, f );
+            dual.shiftMB( fMB, f );
+            dual.shiftMC( fMC, f );
+
+            const V3 vf = dual.vertices[iff];
+
+            if(fMA[2]!=PatchIdxVoid){
+              const Idx ifMA = dual.idx(fMA);
+              const V3 vfMA = dual.vertices[ifMA];
+              const double dx = (vf-vfMA).norm();
+              std::cout << "-A x,y,s = " << fMA[0] << " " << fMA[1] << " " << fMA[2] << " " << fMA[3] << std::endl;
+              std::cout << dx << std::endl;
+              // assert( dx<0.7 );
+            }
+            if(fMB[2]!=PatchIdxVoid){
+              const Idx ifMB = dual.idx(fMB);
+              const V3 vfMB = dual.vertices[ifMB];
+              const double dy = (vf-vfMB).norm();
+              std::cout << "-B x,y,s = " << fMB[0] << " " << fMB[1] << " " << fMB[2] << " " << fMB[3] << std::endl;
+              std::cout << dy << std::endl;
+              // assert( dy<0.7 );
+            }
+            if(fMC[2]!=PatchIdxVoid){
+              const Idx ifMC = dual.idx(fMC);
+              const V3 vfMC = dual.vertices[ifMC];
+              const double dz = (vf-vfMC).norm();
+              std::cout << "-C x,y,s = " << fMC[0] << " " << fMC[1] << " " << fMC[2] << " " << fMC[3] << std::endl;
+              std::cout << dz << std::endl;
+              // assert( dz<0.7 );
+            }
+          }
+        }}}
+  }
+
+
+
+
+
+
   {
     // for dual
     Orbits orbits(dual.vertices, lattice, Ih, rot);
