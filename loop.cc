419962c99ee72b182893507822dba0290c6bf96c
Nobuyuki Matsumoto <nobuyukimatsumoto@Nobuyukis-MacBook-Pro.local>
2025-12-04 22:09:57 -0500

    visu

--- loop.cc
+++ loop.cc
@@ -36,13 +36,11 @@ using Complex = std::complex<double>;
 
 
 // #include <omp.h>
-
-
-constexpr int L = 1;
-constexpr Idx N = 10*L*L+2;
-#include <bitset>
-using SimpConfig = std::bitset<N>;
 #include "loop.h"
+
+constexpr int L = 2;
+constexpr Idx N = 10*L*L+2;
+
 
 
 
@@ -60,140 +58,54 @@ int main(int argc, char* argv[]){
 
   RefinedIcosahedron lattice(L);
   assert(lattice.NVertices()==N);
-  FullIcosahedralGroup Ih( "multtablemathematica.dat", 3, 19, 60 );
-  Rotation rot;
+  // FullIcosahedralGroup Ih( "multtablemathematica.dat", 3, 19, 60 );
+  // Rotation rot;
   RefinedIcosahedronDual dual(lattice);
-
-  // std::cout << "len = " << arcLength( lattice.vertices[0], lattice.vertices[1] ) << std::endl;
-  // std::cout << "len = " << arcLength( dual.vertices[0], dual.vertices[1] ) << std::endl;
-  // std::cout << "len = " << arcLength( dual.vertices[0], dual.vertices[2] ) << std::endl;
-  // std::cout << "len = " << arcLength( dual.vertices[0], dual.vertices[3] ) << std::endl;
-
-  // std::cout << "faces = " << std::endl;
-  // for(auto& face : dual.faces){
-  //   std::cout << "length = " << face.size() << std::endl;
-  //   for(auto& elem : face){
-  //     FaceCoords f = dual.idx2FaceCoords(elem.first);
-  //     std::cout << "debug. f = " << f[0] << " " << f[1] << " " << f[2] << " " << f[3] << std::endl;
-  //     std::cout << "debug. df = " << elem.second << std::endl;
-  //   }
-  // }
-
-
-  // Orbits orbits(dual.vertices,
-  //               dual.basePoints,
-  //               dual.baseTypes,
-  //               lattice, Ih, rot);
 
   DualSpinStructure spin(dual);
 
-  // DualLoopUtils loops;
+  DualLoops<N> loops(dual);
   {
     Idx q = std::pow(2, k) + r; // Binary: 00100101
+    loops.set( q );
 
-    SimpConfig c( q );
+    std::cout << "idx = " << loops() << std::endl;
 
-    std::cout << "c = " << c << std::endl;
-    // for(Idx in=0; in<N; in++) std::cout << c[in] << std::endl;
-    // std::cout << std::endl;
-
-    // std::vector<Idx> ups;
-    std::set<Idx> ups;
-    // 1:up, 0:down
-    for(Idx in=0; in<N; in++) {
-      if(c[in]==1) ups.insert(in);
-    }
-
-    std::set<Link> links_in_loops;
-    for(const Idx in : ups){
-      const Coords n = lattice.idx2Coords(in);
-      // std::cout << "debug. in = " << in << " n = " << n[0] << " " << n[1] << " " << n[2] << std::endl;
-
-      for(int mu=0; mu<6; mu++){
-        // std::cout << "debug. mu = " << mu << std::endl;
-        Coords np;
-        lattice.shift( np, n, mu );
-        if(np[2]==PatchIdxVoid) continue;
-        const Idx inp = lattice.idx(np);
-
-        if( !ups.contains(inp) ){ // (n, np) : broken
-          // std::cout << "debug. inp = " << inp << " np = " << np[0] << " " << np[1] << " " << np[2] << std::endl;
-
-          const DirectedLink duallink = dual.linkSimp2Dual.at( DirectedLink{in,mu} );
-          // const FaceCoords fA = dual.idx2FaceCoords( duallink.first );
-          const FaceCoords fA = dual.idx2FaceCoords( duallink.first );
-          const int df = duallink.second;
-          FaceCoords fB;
-          dual.shift( fB, fA, df );
-
-          // dual.get_dual_link( fA, fB, n, mu );
-          // std::cout << "debug. fA = " << fA[0] << " " << fA[1] << " " << fA[2] << " " << fA[3] << std::endl;
-          // std::cout << "debug. fB = " << fB[0] << " " << fB[1] << " " << fB[2] << " " << fB[3] << std::endl;
-
-          const Idx ifA = dual.idx(fA);
-          const Idx ifB = dual.idx(fB);
-          links_in_loops.insert( Link{ifA, ifB} );
-          // if(ifA<ifB) ;
-          // else links_in_loops.insert( Link{ifB, ifA} );
-        }
-      }
-    }
-    std::cout << "links_in_loops = " << std::endl;
-    for(auto elem : links_in_loops) std::cout << elem.first << " " << elem.second << std::endl;
-    std::cout << std::endl;
-
-    // std::set<Idx> sites_in_loops;
-    // for(auto elem : links_in_loops) {
-    //   sites_in_loops.insert(elem.first);
-    //   sites_in_loops.insert(elem.second);
-    // }
-
-    // group links
-    using Loop = std::vector<Idx>; //  link;
-    std::vector<Loop> loops;
-
-
-    while(links_in_loops.size()!=0){
-      // initialize a loop
-      Loop loop;
-      const Link ell = *links_in_loops.begin();
-      const Idx if_end = ell.first, if_init = ell.second;
-
-      // retrieve a loop starting from if_init; coming back to if_end
-      Idx if_current = if_init;
-      loop.push_back(if_current);
-
-      while(*std::prev(loop.end())!=if_end){
-        for(auto em=std::next(links_in_loops.begin()); em!=links_in_loops.end(); em++){
-          // std::cout << "em = " << em->first << " " << em->second << std::endl; // always updated
-          if(em->first==if_current){
-            if_current = em->second;
-            loop.push_back(if_current);
-            // std::cout << "debug. insert second" << std::endl;
-            links_in_loops.erase(em);
-            break;
-          }
-          else if(em->second==if_current){
-            if_current = em->first;
-            loop.push_back(if_current);
-            // std::cout << "debug. insert first" << std::endl;
-            links_in_loops.erase(em);
-            break;
-          }
-        } // end for
-        //  std::cout << "debug. if_current = " << if_current << std::endl;
-      }
-      links_in_loops.erase(ell);
-      loops.push_back(loop);
-    }
+    std::cout << "config = " << loops.config() << std::endl;
 
     std::cout << "loops = " << std::endl;
-    for(auto loop : loops){
-      for(auto elem : loop) std::cout << elem << " ";
-      std::cout << std::endl;
-    }
+    std::cout << loops.printLoops() << std::endl;
 
+
+
+    {
+      double eps = 0.02;
+
+      // std::ofstream ofs("loops"+std::to_string(L)+"_"+std::to_string(q)+".dat");
+      std::ofstream ofs("loops.dat");
+      ofs << std::scientific << std::setprecision(25);
+
+      for(const auto& loop : loops.loops){
+        for(Idx i1=0; i1<loop.size(); i1++){
+          const Idx i2 = (i1+1)%loop.size();
+
+          const Idx if1 = loop[i1];
+          const Idx if2 = loop[i2];
+          std::cout << if1 << " " << if2 << std::endl;
+
+          const V3 rf1 = dual.vertices[if1];
+          const V3 rf2 = dual.vertices[if2];
+
+          for(double t=0.0; t<1.0; t+=eps){
+            V3 p = spin.p(rf1, rf2, t);
+            ofs << p.transpose() << std::endl;
+          }
+        }}
+
+    }
   }
+
+  
 
   return 0;
 }
--- loop.h
+++ loop.h
@@ -1,11 +1,119 @@
 #pragma once
 
 
+#include <bitset>
+#include <set>
 
-struct DualLoopUtils {
 
-  DualLoopUtils(){
+template<Idx N>
+class DualLoops {
+public:
+  using Config = std::bitset<N>;
+  using Loop = std::vector<Idx>;
+
+  const RefinedIcosahedronDual& dual;
+  const RefinedIcosahedron& lattice;
+
+  Idx q;
+  Config c;
+
+  std::vector<Loop> loops;
+  // auto begin(){ return loops.begin(); }
+  // auto end(){ return loops.end(); }
+  // auto begin() const { return loops.begin(); }
+  // auto end() const { return loops.end(); }
+
+  DualLoops
+  (
+   const RefinedIcosahedronDual& dual_
+   )
+    : dual(dual_)
+    , lattice(dual.simplicial)
+  {}
+
+  Idx operator()() const { return q; }
+
+  bool operator[](const Idx i) const {
+    assert(i<N);
+    return c[i];
   }
 
-  
+  Config config() const { return c; }
+
+  std::string printLoops() const {
+    std::stringstream ss;
+    for(auto loop : loops){
+      for(auto elem : loop) ss << elem << " ";
+      ss << std::endl;
+    }
+    return ss.str();
+  }
+
+
+  void set( const Idx q_ ){
+    q = q_;
+    //    c.
+    c = Config( q );
+
+    loops.clear();
+
+    // 1:up, 0:down
+    std::set<Idx> ups;
+    for(Idx in=0; in<N; in++) if(c[in]==1) ups.insert(in);
+
+    std::set<Link> links_in_loops;
+    for(const Idx in : ups){
+      const Coords n = lattice.idx2Coords(in);
+
+      for(int mu=0; mu<6; mu++){
+        Coords np;
+        lattice.shift( np, n, mu );
+        if(np[2]==PatchIdxVoid) continue;
+        const Idx inp = lattice.idx(np);
+
+        if( !ups.contains(inp) ){ // (n, np) : broken
+          const DirectedLink duallink = dual.linkSimp2Dual.at( DirectedLink{in,mu} );
+          const FaceCoords fA = dual.idx2FaceCoords( duallink.first );
+          const int df = duallink.second;
+          FaceCoords fB;
+          dual.shift( fB, fA, df );
+
+          const Idx ifA = dual.idx(fA);
+          const Idx ifB = dual.idx(fB);
+          links_in_loops.insert( Link{ifA, ifB} );
+        }
+      }
+    }
+
+    // group links
+    while(links_in_loops.size()!=0){
+      // initialize a loop
+      Loop loop;
+      const Link ell = *links_in_loops.begin();
+      const Idx if_end = ell.first, if_init = ell.second;
+
+      // retrieve a loop starting from if_init; coming back to if_end
+      Idx if_current = if_init;
+      loop.push_back(if_current);
+
+      while(*std::prev(loop.end())!=if_end){
+        for(auto em=std::next(links_in_loops.begin()); em!=links_in_loops.end(); em++){
+          if(em->first==if_current){
+            if_current = em->second;
+            loop.push_back(if_current);
+            links_in_loops.erase(em);
+            break;
+          }
+          else if(em->second==if_current){
+            if_current = em->first;
+            loop.push_back(if_current);
+            links_in_loops.erase(em);
+            break;
+          }
+        } // end for
+      }
+      links_in_loops.erase(ell);
+      loops.push_back(loop);
+    }
+  }
 };
